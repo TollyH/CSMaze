17107d4697e6015c490180898ff6d6c719519353
Tolly <34184902+TollyH@users.noreply.github.com>
2022-11-16 22:46:07 +0000

    Implement level json loading/saving

---
+++ CSMaze/MazeLevels.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace CSMaze
+{
+    public static class MazeLevels
+    {
+        public static Level[] LoadLevelJson(string path)
+        {
+            List<JsonLevel>? jsonLevels = JsonConvert.DeserializeObject<List<JsonLevel>>(File.ReadAllText(path));
+            return jsonLevels is null ? Array.Empty<Level>() : jsonLevels.Select(x => x.GetLevel()).ToArray();
+        }
+
+        public static void SaveLevelJson(string path, Level[] levels)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(levels.Select(x => x.GetJsonLevel())));
+        }
+    }
+}
