32bcbf0823c9dd99a0548f0b45e7a5b8143babd0
Tolly <34184902+TollyH@users.noreply.github.com>
2022-11-27 22:37:57 +0000

    Selection and properties panel

--- CSMazeDesigner/MainWindow.xaml.cs
+++ CSMazeDesigner/MainWindow.xaml.cs
@@ -40,13 +40,17 @@ namespace CSMaze.Designer
 
         private readonly Dictionary<Tool, string> descriptions = new();
         private readonly Dictionary<Tool, Button> toolButtons = new();
-        private readonly Dictionary<string, Image> textures = new();
-        private readonly Dictionary<string, Image> decorationTextures = new();
+
+        private WallDirection SelectedDirection => textureDimensionWest.IsChecked!.Value ? WallDirection.West : textureDimensionEast!.IsChecked.Value
+            ? WallDirection.East : textureDimensionSouth!.IsChecked.Value ? WallDirection.South : WallDirection.North;
 
         public MainWindow() : this(null) { }
 
         public MainWindow(string? configIniPath = "config.ini")
         {
+            // Change working directory to the directory where the script is located.
+            // This prevents issues with required files not being found.
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
             cfg = new Config(configIniPath ?? "config.ini");
             InitializeComponent();
             foreach (Button btn in toolButtonPanel.Children.OfType<Button>())
@@ -55,6 +59,27 @@ namespace CSMaze.Designer
             }
             mapCanvas.Width = cfg.ViewportWidth + 50;
             mapCanvas.Height = cfg.ViewportHeight + 50;
+            string[] descLines = File.ReadAllLines("level_designer_descriptions.txt");
+            foreach (string descLine in descLines)
+            {
+                string[] split = descLine.Split('|');
+                descriptions[Enum.Parse<Tool>(split[0])] = split[1];
+            }
+            foreach (string filepath in Directory.EnumerateFiles(System.IO.Path.Join("textures", "wall"), "*.png"))
+            {
+                _ = textureDropdown.Items.Add(new ComboBoxItem() { Content = filepath.Split("\\")[^1].Split(".")[0] });
+                _ = edgeTextureDropdown.Items.Add(new ComboBoxItem() { Content = filepath.Split("\\")[^1].Split(".")[0] });
+            }
+            foreach (string filepath in Directory.EnumerateFiles(System.IO.Path.Join("textures", "sprite", "decoration"), "*.png"))
+            {
+                _ = decorationTextureDropdown.Items.Add(new ComboBoxItem() { Content = filepath.Split("\\")[^1].Split(".")[0] });
+            }
+            texturePreview.Width = MazeGame.TextureWidth;
+            texturePreview.Height = MazeGame.TextureHeight;
+            edgeTexturePreview.Width = MazeGame.TextureWidth;
+            edgeTexturePreview.Height = MazeGame.TextureHeight;
+            decorationTexturePreview.Width = MazeGame.TextureWidth;
+            decorationTexturePreview.Height = MazeGame.TextureHeight;
         }
 
         /// <summary>
@@ -115,7 +140,7 @@ namespace CSMaze.Designer
                 unsavedChanges = false;
                 UpdateLevelList();
                 UpdateMapCanvas();
-                // TODO: UpdatePropertiesPanel()
+                UpdatePropertiesPanel();
             }
             catch (Exception exc)
             {
@@ -324,6 +349,205 @@ namespace CSMaze.Designer
             doUpdates = true;
         }
 
+        /// <summary>
+        /// Update the properties panel with information about the selected tile.
+        /// Also updates the width and height sliders.
+        /// </summary>
+        private void UpdatePropertiesPanel()
+        {
+            if (!doUpdates)
+            {
+                return;
+            }
+            if (currentLevel < 0)
+            {
+                selectedSquareDescription.Background = null;
+                selectedSquareDescription.Foreground = Brushes.Black;
+                selectedSquareDescription.Text = "Nothing is currently selected";
+                dimensionsPanel.Visibility = Visibility.Collapsed;
+                monsterWaitPanel.Visibility = Visibility.Collapsed;
+                texturesPanel.Visibility = Visibility.Collapsed;
+                edgeTexturesPanel.Visibility = Visibility.Collapsed;
+                decorationTexturesPanel.Visibility = Visibility.Collapsed;
+                return;
+            }
+            dimensionsPanel.Visibility = Visibility.Visible;
+            Level lvl = levels[currentLevel];
+            doUpdates = false;
+            widthDimensionLabel.Content = $"Level width - ({lvl.Dimensions.Width})";
+            widthDimensionSlider.Value = lvl.Dimensions.Width;
+            heightDimensionLabel.Content = $"Level height - ({lvl.Dimensions.Height})";
+            heightDimensionSlider.Value = lvl.Dimensions.Height;
+            doUpdates = true;
+            // Remove all property widgets that apply to only a certain type of grid square.
+            monsterWaitPanel.Visibility = Visibility.Collapsed;
+            texturesPanel.Visibility = Visibility.Collapsed;
+            edgeTexturesPanel.Visibility = Visibility.Collapsed;
+            decorationTexturesPanel.Visibility = Visibility.Collapsed;
+            if (currentTile.X == -1 || currentTile.Y == -1)
+            {
+                selectedSquareDescription.Background = null;
+                selectedSquareDescription.Foreground = Brushes.Black;
+                selectedSquareDescription.Text = "Nothing is currently selected";
+            }
+            else if (lvl.OriginalExitKeys.Contains(currentTile))
+            {
+                selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.Gold.R, ScreenDrawing.Gold.G, ScreenDrawing.Gold.B));
+                selectedSquareDescription.Foreground = Brushes.Black;
+                selectedSquareDescription.Text = descriptions[Tool.Key];
+            }
+            else if (lvl.OriginalKeySensors.Contains(currentTile))
+            {
+                selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.DarkGold.R, ScreenDrawing.DarkGold.G, ScreenDrawing.DarkGold.B));
+                selectedSquareDescription.Foreground = Brushes.White;
+                selectedSquareDescription.Text = descriptions[Tool.Sensor];
+            }
+            else if (lvl.OriginalGuns.Contains(currentTile))
+            {
+                selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.Grey.R, ScreenDrawing.Grey.G, ScreenDrawing.Grey.B));
+                selectedSquareDescription.Foreground = Brushes.Black;
+                selectedSquareDescription.Text = descriptions[Tool.Gun];
+            }
+            else if (lvl.Decorations.ContainsKey(currentTile))
+            {
+                selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.Purple.R, ScreenDrawing.Purple.G, ScreenDrawing.Purple.B));
+                selectedSquareDescription.Foreground = Brushes.White;
+                selectedSquareDescription.Text = descriptions[Tool.Decoration];
+                decorationTexturesPanel.Visibility = Visibility.Visible;
+                doUpdates = false;
+                decorationTextureDropdown.Text = lvl.Decorations[currentTile];
+                doUpdates = true;
+                decorationTexturePreview.Source = new BitmapImage(
+                    new Uri(System.IO.Path.Join(AppDomain.CurrentDomain.BaseDirectory, "textures", "sprite", "decoration", lvl.Decorations[currentTile]) + ".png"));
+            }
+            else if (lvl.MonsterStart == currentTile)
+            {
+                selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.DarkRed.R, ScreenDrawing.DarkRed.G, ScreenDrawing.DarkRed.B));
+                selectedSquareDescription.Foreground = Brushes.White;
+                selectedSquareDescription.Text = descriptions[Tool.Monster];
+                monsterWaitPanel.Visibility = Visibility.Visible;
+                if (lvl.MonsterWait is not null)
+                {
+                    doUpdates = false;
+                    monsterWaitLabel.Content = $"Monster spawn time - ({Math.Round(lvl.MonsterWait.Value)})";
+                    monsterWaitSlider.Value = lvl.MonsterWait.Value / 5;
+                    doUpdates = true;
+                }
+            }
+            else if (lvl.StartPoint == currentTile)
+            {
+                selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.Red.R, ScreenDrawing.Red.G, ScreenDrawing.Red.B));
+                selectedSquareDescription.Foreground = Brushes.White;
+                selectedSquareDescription.Text = descriptions[Tool.Start];
+            }
+            else if (lvl.EndPoint == currentTile)
+            {
+                selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.Green.R, ScreenDrawing.Green.G, ScreenDrawing.Green.B));
+                selectedSquareDescription.Foreground = Brushes.Black;
+                selectedSquareDescription.Text = descriptions[Tool.End];
+            }
+            else
+            {
+                if (lvl[currentTile].Wall is null)
+                {
+                    selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.White.R, ScreenDrawing.White.G, ScreenDrawing.White.B));
+                    selectedSquareDescription.Foreground = Brushes.Black;
+                    selectedSquareDescription.Text = descriptions[Tool.Select];
+                    edgeTexturesPanel.Visibility = Visibility.Visible;
+                    doUpdates = false;
+                    edgeTextureDropdown.Text = lvl.EdgeWallTextureName;
+                    doUpdates = true;
+                    edgeTexturePreview.Source = new BitmapImage(
+                        new Uri(System.IO.Path.Join(AppDomain.CurrentDomain.BaseDirectory, "textures", "wall", lvl.EdgeWallTextureName) + ".png"));
+                }
+                else
+                {
+                    selectedSquareDescription.Background = new SolidColorBrush(Color.FromRgb(ScreenDrawing.Black.R, ScreenDrawing.Black.G, ScreenDrawing.Black.B));
+                    selectedSquareDescription.Foreground = Brushes.White;
+                    selectedSquareDescription.Text = descriptions[Tool.Wall];
+                    texturesPanel.Visibility = Visibility.Visible;
+                    textureDimensionNorth.IsChecked = true;
+                    (string, string, string, string)? tileTextures = lvl[currentTile].Wall;
+                    if (tileTextures is not null)
+                    {
+                        string texture = SelectedDirection == WallDirection.North ? tileTextures.Value.Item1 : SelectedDirection == WallDirection.East
+                            ? tileTextures.Value.Item2 : SelectedDirection == WallDirection.South ? tileTextures.Value.Item3 : tileTextures.Value.Item4;
+                        doUpdates = false;
+                        textureDropdown.Text = texture;
+                        doUpdates = true;
+                        texturePreview.Source = new BitmapImage(
+                            new Uri(System.IO.Path.Join(AppDomain.CurrentDomain.BaseDirectory, "textures", "wall", texture) + ".png"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when the map canvas is clicked by the user or the mouse is moved while the left mouse button is held down.
+        /// Handles the event based on the currently selected tool.
+        /// </summary>
+        /// <param name="e">The event args from the original mouse event</param>
+        private void CanvasMouseEvent(MouseEventArgs e, bool wasClick)
+        {
+            if (currentLevel < 0)
+            {
+                return;
+            }
+            Level lvl = levels[currentLevel];
+            int tileWidth = (int)mapCanvas.ActualWidth / (int)Math.Max(lvl.Dimensions.Width * zoomLevel, 1);
+            int tileHeight = (int)mapCanvas.ActualHeight / (int)Math.Max(lvl.Dimensions.Height * zoomLevel, 1);
+            Point mousePos = e.GetPosition(mapCanvas);
+            System.Drawing.Point clickedTile = new((int)(mousePos.X / (tileWidth - 1)) + scrollOffset.X, (int)(mousePos.Y / (tileHeight - 1)) + scrollOffset.Y);
+            if (!lvl.IsCoordInBounds(clickedTile))
+            {
+                return;
+            }
+            if (!wasClick && clickedTile == lastVisitedTile)
+            {
+                return;
+            }
+            lastVisitedTile = clickedTile;
+            switch (currentTool)
+            {
+                case Tool.Select:
+                    currentTile = clickedTile;
+                    if (lvl[clickedTile].Wall is not null)
+                    {
+                        bulkWallSelection.Add(currentTile);
+                    }
+                    else if (wasClick)
+                    {
+                        bulkWallSelection.Clear();
+                    }
+                    break;
+                case Tool.Move:
+                    break;
+                case Tool.Wall:
+                    break;
+                case Tool.CollisionPlayer:
+                    break;
+                case Tool.CollisionMonster:
+                    break;
+                case Tool.Start:
+                    break;
+                case Tool.End:
+                    break;
+                case Tool.Key:
+                    break;
+                case Tool.Sensor:
+                    break;
+                case Tool.Gun:
+                    break;
+                case Tool.Monster:
+                    break;
+                case Tool.Decoration:
+                    break;
+                default: break;
+            }
+            UpdateMapCanvas();
+            UpdatePropertiesPanel();
+        }
+
         private void ToolButton_Click(object sender, RoutedEventArgs e)
         {
             SelectTool((Tool)((Button)sender).Tag);
@@ -335,11 +559,11 @@ namespace CSMaze.Designer
             {
                 SelectTool(currentTool + 1);
             }
-            if (e.Key == Key.W)
+            else if (e.Key == Key.W)
             {
                 SelectTool(currentTool - 1);
             }
-            if (e.Key == Key.A)
+            else if (e.Key == Key.A)
             {
                 // TODO: Bulk select walls
             }
@@ -347,12 +571,15 @@ namespace CSMaze.Designer
 
         private void mapCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            CanvasMouseEvent(e, true);
         }
 
         private void mapCanvas_MouseMove(object sender, MouseEventArgs e)
         {
-
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                CanvasMouseEvent(e, false);
+            }
         }
 
         private void zoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -451,7 +678,7 @@ namespace CSMaze.Designer
                 zoomSlider.Value = 1;
                 doUpdates = true;
                 UpdateMapCanvas();
-                // TODO: UpdatePropertiesFrame();
+                UpdatePropertiesPanel();
             }
         }
     }
