99b6041cf9fc64f9b3439ae080ef1e51a6e796ba
Tolly <34184902+TollyH@users.noreply.github.com>
2022-11-27 00:59:09 +0000

    Designer tool buttons

--- CSMazeDesigner/MainWindow.xaml.cs
+++ CSMazeDesigner/MainWindow.xaml.cs
@@ -20,9 +20,53 @@ namespace CSMaze.Designer
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly Config cfg;
+        private string? currentPath = null;
+        private Level[] levels = Array.Empty<Level>();
+        private int currentLevel = -1;
+        private Tool currentTool = Tool.Select;
+        private System.Drawing.Point currentTile = new(-1, -1);
+        private List<System.Drawing.Point> bulkWallSelection = new();
+        // The mouse must leave a tile before that same tile is modified again.
+        private System.Drawing.Point lastVisitedTile = new(-1, -1);
+        private double zoomLevel = 1;
+        private System.Drawing.Point scrollOffset = new(0, 0);
+        private List<(int, Level[])> undoStack = new();
+        private bool unsavedChanges = false;
+        // Used to prevent methods from being called when programmatically setting widget values.
+        private bool doUpdates = true;
+
+        private Dictionary<Tool, string> descriptions;
+        private Dictionary<string, Image> textures;
+        private Dictionary<string, Image> decorationTextures;
+
+        public MainWindow() : this(null) { }
+
+        public MainWindow(string? configIniPath = "config.ini")
         {
+            cfg = new Config(configIniPath ?? "config.ini");
             InitializeComponent();
         }
+
+        private void ToolButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public enum Tool
+    {
+        Select,
+        Move,
+        Wall,
+        CollisionPlayer,
+        CollisionMonster,
+        Start,
+        End,
+        Key,
+        Sensor,
+        Gun,
+        Monster,
+        Decoration
     }
 }
